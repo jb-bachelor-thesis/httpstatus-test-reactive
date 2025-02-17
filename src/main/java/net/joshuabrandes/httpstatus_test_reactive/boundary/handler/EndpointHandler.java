6e78c3cbd7b7bc4cae8874c6938c3a552a93bdbd
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2025-02-17 12:25:15 +0100

    switching to Mono.dalay due to rate limit

--- src/main/java/net/joshuabrandes/httpstatus_test_reactive/boundary/handler/EndpointHandler.java
+++ src/main/java/net/joshuabrandes/httpstatus_test_reactive/boundary/handler/EndpointHandler.java
@@ -1,25 +1,17 @@
 package net.joshuabrandes.httpstatus_test_reactive.boundary.handler;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Component
 public class EndpointHandler {
 
-    private final WebClient webClient;
-
-    public EndpointHandler(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://httpstat.us").build();
-    }
-
     public Mono<ServerResponse> getStatus() {
-        return webClient.get()
-                .uri("/200?sleep=5000")
-                .retrieve()
-                .bodyToMono(String.class)
-                .flatMap(responseBody -> ServerResponse.ok().bodyValue(responseBody))
-                .onErrorResume(_ -> ServerResponse.status(500).bodyValue("Internal Server Error"));
+        // sleep for 5 seconds and then return 200 with Body "OK"
+        return Mono.delay(Duration.ofSeconds(5))
+                .then(ServerResponse.ok().bodyValue("OK"));
     }
 }
